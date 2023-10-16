f2e931076ee96f1b41956cbb65a5e94ce7439940
dustech <stefano.cerruti@outlook.com>
2023-10-17 01:06:32 +0200

    cavolo se e' difficile

--- Chapter1/Main.hs
+++ Chapter1/Main.hs
@@ -1,3 +1,33 @@
+-- puoi lanciare anche facendo runhaskell + nome file
+
 module Main where
 
-main = print "Hello, World!"
+-- main = print "Hello, World!"
+
+
+main = do
+  --putStrLn greeting 
+  putStr (show 666)
+  print "Mi piace la salsiccia!"
+
+saluta primaPersona secondaPersona = primaPersona <> " dice ciao a " <> secondaPersona
+
+salutaInfix secondaPersona = (`saluta` secondaPersona)
+
+-- salutation = "Hello"
+-- person = "Bongo"
+
+-- greeting =
+--   salutation <> " " <> person
+
+-- message = "topo"
+
+-- makeGreeting salutation' person' = salutation' <> " " <> person'
+
+-- salutaDue = (`makeGreeting` "Due")
+-- salutaDue' x = x (`makeGreeting` "Due")
+-- salutaDue'' x = makeGreeting x "Due"
+
+-- mkGreInfix x y = x `makeGreeting` y
+-- mkGreInfix' = ("bongo" `makeGreeting`)
+
