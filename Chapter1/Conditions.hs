a66f98813a129350564b7aaee7393a7a89b1cbcb
Stefano Cerruti <stefano.cerruti@outlook.com>
2023-10-20 12:23:25 +0200

    conditions

---
+++ Chapter1/Conditions.hs
@@ -0,0 +1,17 @@
+module Main where
+
+handleSmallNumber num =
+  if num < 10
+  then print num
+  else print "il numero e' troppo grande!"
+
+guardSize num
+  | num < 3 = "numero piccino"
+  | num < 10 = "numero medio"
+  | num < 100 = "numero cicciotto"
+  | num < 1000 = "numero molto grosso!"
+  | otherwise = "numero immenso!!"
+
+bonzo myBonzo | otherwise = "Bonzo sei " <> myBonzo
+
+main = putStr "main run"
