a031336e4821b1ed434fb8a7afe43969cdb34d34
Stefano Cerruti <stefano.cerruti@outlook.com>
2023-10-21 09:56:50 +0200

    Aggiornamento al Sat Oct 21 09:56:47 AM CEST 2023

--- Chapter2/Lists.hs
+++ Chapter2/Lists.hs
@@ -5,4 +5,27 @@ myHead = head myList
 myTail = tail myList
 
 
+factors num =
+  factors' num 2
+  where 
+    factors' num fact
+      | num == 1 = []
+      | (num `rem` fact) == 0 = fact:factors' (num `div` fact) fact
+      | otherwise = factors' num (fact + 1)
+
+{- 
+computazione:
+
+f 10?
+
+f' 10 2
+
+10 r 2 = 0 -> 2: f' 5 2
+  5 r 2 <> 0 -> f' 5 3
+  ...
+  5 r 5 = 0 -> 5: f' 1 5
+  end
+ricostruisce 2:5:[]
+
+-}
 
