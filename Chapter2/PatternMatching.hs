f984da6dd4e1eb258ffd8fd0993151e9b995916a
Stefano Cerruti <stefano.cerruti@outlook.com>
2023-10-22 21:09:15 +0200

    pattern match

--- Chapter2/PatternMatching.hs
+++ Chapter2/PatternMatching.hs
@@ -1,3 +1,11 @@
 customGreeting "George" = "Oh, hey George!"
 customGreeting "Mungo" = "Oh, hey Mungo! Grondo"
 customGreeting name = "Buongiorno " <>  name
+
+
+addValues [] = 0
+addValues (first:rest) = first + (addValues rest)
+
+printHead [] = "vuoto di brutto"
+printHead lst@(head:_tail) =
+  "la testa di " <> show lst <> " e' " <> show head
