758d68591f12c8b2611d994ac25d839b710a3b74
Stefano Cerruti <stefano.cerruti@outlook.com>
2023-10-24 23:33:20 +0200

    altri esercizi

--- Chapter2/Exercises.hs
+++ Chapter2/Exercises.hs
@@ -17,3 +17,20 @@ invertir :: Foldable t => t a -> [a]
 invertir = foldr aggrer []
   where
     aggrer x y = [x] <> y
+
+zippy [] _ = []
+zippy _ [] = []
+zippy (x:xs) (y:ys) =
+  [(x,y)] <> zippy xs ys
+
+zippyc [] _ _ = []
+zippyc _ [] _ = []
+zippyc (x:xs) (y:ys) connector =
+  [connector x y] <> zippyc xs ys connector
+  
+
+zippyco xs ys = [(x,y) | x <- xs, y <- ys]
+
+-- pairs' as bs = [(a, b) | a <- as, a `elem` bs, b <- bs, odd b]
+
+
