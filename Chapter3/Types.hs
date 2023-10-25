57263988305607a7d37cf7eba12ce2719f7a5018
Stefano Cerruti <stefano.cerruti@outlook.com>
2023-10-25 10:02:00 +0200

    types

---
+++ Chapter3/Types.hs
@@ -0,0 +1,22 @@
+
+dammiWord :: Word
+dammiWord = 1234
+
+sottrai :: Word
+sottrai = dammiWord - 3000
+
+myD :: Float
+myD = 12.45
+
+myA :: Char
+myA = 'a'
+
+myS :: String
+myS = "a"
+
+myAs :: String
+myAs = [myA] <> myS
+
+myAs' :: String
+myAs' = myA : myS
+
